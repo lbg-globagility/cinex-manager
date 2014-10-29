954dd9485d86602af80769b5d1e32548a1d1bfc4
Reymond Ong <reymond.ong@amellar.com>
2014-10-29 11:27:37 +0800

    Allow enter key to move to password element in login page.

--- aZynEManager/Cinemapps/MainWindow.xaml.cs
+++ aZynEManager/Cinemapps/MainWindow.xaml.cs
@@ -591,6 +591,15 @@ namespace Cinemapps
                 LoginClick();
             }
         }
+
+        private void UserName_KeyUp(object sender, System.Windows.Input.KeyEventArgs e)
+        {
+            if (e.Key == Key.Return)
+            {
+                if (UserPassword.Focusable)
+                    Keyboard.Focus(UserPassword);
+            }
+        }
     }
     
 }
