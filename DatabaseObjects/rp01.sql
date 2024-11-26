32478ab44576b2ba9b732caa77c24f32e1dc6c81
Lambert Galit <lbg.globagility@gmail.com>
2024-11-26 10:58:13 +0800

    fix: tiny changes to resolve issue on RP01

--- DatabaseObjects/rp01.sql
+++ DatabaseObjects/rp01.sql
@@ -18,15 +18,26 @@ DROP TEMPORARY TABLE IF EXISTS `tbl_rp01`;
 CREATE TEMPORARY TABLE IF NOT EXISTS `tbl_rp01`
 SELECT
 
-#u.id user, p.code, p.name `PATRON`, c.name `CINEMA`, mslrs.base_price `PRICE`, COUNT(mslrs.cinema_seat_id) `QTY`, SUM(mslrs.base_price) `TOTALSALES`, SUM(mslrs.ordinance_price) `TOTALORDINANCE`, SUM(mslrs.surcharge_price) `TOTALSURCHARGE`, SUM(mslrs.base_price) + SUM(mslrs.ordinance_price) + SUM(mslrs.surcharge_price) `GRANDTOTAL`, ms.movie_date, t.ticket_datetime, u.userid, ct.system_value, r.name reportname, SUM(IFNULL(sur.amount_val, 0)) `FOOD`
-
-u.id user, p.code, p.name `PATRON`, c.name `CINEMA`, mslrs.base_price `PRICE`, (mslrs.cinema_seat_id) `QTY`, (mslrs.base_price) `TOTALSALES`, (mslrs.ordinance_price) `TOTALORDINANCE`,
-#(mslrs.surcharge_price) `TOTALSURCHARGE`,
-(mslrs.surcharge_price - IFNULL(sur.amount_val, 0)) `TOTALSURCHARGE`,
-#IFNULL(sur.amount_val, 0) `TOTALSURCHARGE`,
-(mslrs.base_price) + (mslrs.ordinance_price) + (mslrs.surcharge_price) `GRANDTOTAL`, ms.movie_date, t.ticket_datetime, u.userid, ct.system_value, r.name `reportname`, (IFNULL(i.amount_val, 0)) `FOOD`, p.id `PatronId`, c.in_order `CinemaOrdinal`
-
-#, mslrs.*
+u.id `user`,
+p.`code`,
+p.`name` `PATRON`,
+c.`name` `CINEMA`,
+mslrs.base_price `PRICE`,
+mslrs.cinema_seat_id `QTY`,
+mslrs.base_price `TOTALSALES`,
+mslrs.ordinance_price `TOTALORDINANCE`,
+#IFNULL(ps.PremiumSeatTotal, 0) + IFNULL(ps.SurchargeTotal, 0) `TOTALSURCHARGE`,
+IFNULL(ps.GrandTotalSurcharge, 0) - IFNULL(ps.FoodBundleTotal, 0) `TOTALSURCHARGE`,
+(mslrs.base_price + mslrs.ordinance_price + mslrs.surcharge_price) `GRANDTOTAL`,
+ms.movie_date,
+t.ticket_datetime,
+u.userid,
+ct.system_value,
+r.`name` `reportname`,
+IFNULL(ps.FoodBundleTotal, 0) `FOOD`,
+p.id `PatronId`,
+c.in_order `CinemaOrdinal`,
+mslrs.surcharge_price
 
 FROM movies_schedule_list_reserved_seat mslrs
 INNER JOIN movies_schedule_list msl ON msl.id=mslrs.movies_schedule_list_id
@@ -41,25 +52,23 @@ LEFT JOIN movies_schedule_list_patron mslp ON mslp.id=mslrs.patron_id
 LEFT JOIN patrons p ON p.id=mslp.patron_id
 LEFT JOIN (SELECT
 	ps.*,
-	SUM(sur.amount_val) `amount_val`
+	SUM(sc.amount_val) `GrandTotalSurcharge`,
+	SUM(IFNULL(food.amount_val, 0)) `FoodBundleTotal`,
+	SUM(IFNULL(prem.amount_val, 0)) `PremiumSeatTotal`,
+	SUM(IFNULL(sur.amount_val, 0)) `SurchargeTotal`
 	FROM patrons_surcharge ps
-	INNER JOIN surcharge_tbl sur ON sur.id=ps.surcharge_id AND sur.details LIKE '%FOOD%'
-	GROUP BY ps.patron_id) i ON i.patron_id=p.id
+	INNER JOIN surcharge_tbl sc ON sc.id=ps.surcharge_id
+	LEFT JOIN surcharge_tbl food ON food.id=ps.surcharge_id AND food.details LIKE '%FOOD%'
+	LEFT JOIN surcharge_tbl prem ON prem.id=ps.surcharge_id AND prem.details LIKE '%PREMIUM SEAT%'
+	LEFT JOIN surcharge_tbl sur ON sur.id=ps.surcharge_id AND sur.details='SURCHARGE' AND sur.`code`='SCHARGE'
+	GROUP BY ps.patron_id) ps ON ps.patron_id=p.id
 
-LEFT JOIN (SELECT
-	ps.*,
-	SUM(sur.amount_val) `amount_val`
-	FROM patrons_surcharge ps
-	INNER JOIN surcharge_tbl sur ON sur.id=ps.surcharge_id AND sur.details LIKE '%SURCHARGE%'
-	GROUP BY ps.patron_id) sur ON sur.patron_id=p.id
+#LEFT JOIN surcharge_tbl sur ON sur.details='SURCHARGE' AND sur.`code`='SCHARGE' AND p.with_surcharge=TRUE #AND IFNULL(ps.`SurchargeTotal`, 0) > 0
 
 INNER JOIN config_table ct ON ct.system_code='001'
 INNER JOIN report r ON r.id=1
 
-#WHERE mslrs.id IS NOT NULL
-
-#GROUP BY p.id
-#ORDER BY c.in_order, p.`code`
+WHERE mslrs.`status`=1
 ;
 
 
@@ -80,13 +89,25 @@ INNER JOIN report r ON r.id=1
 
 SELECT
 
-`user`, `code`, `PATRON`, `CINEMA`, `PRICE`, COUNT(i.`QTY`) `QTY`,
+`user`,
+`code`,
+`PATRON`,
+`CINEMA`,
+`PRICE`,
+COUNT(i.`QTY`) `QTY`,
 SUM(i.`TOTALSALES`) `TOTALSALES`,
-
-SUM(i.`TOTALORDINANCE`) `TOTALORDINANCE`, COUNT(i.`QTY`) * SUM(i.`TOTALSURCHARGE`) `TOTALSURCHARGE`, SUM(i.`GRANDTOTAL`) `GRANDTOTAL`, `movie_date`, `ticket_datetime`, `userid`, `system_value`, `reportname`, (COUNT(i.`QTY`) * `FOOD`) `FOOD`, `PatronId`, `CinemaOrdinal`
-
-
-
+SUM(i.`TOTALORDINANCE`) `TOTALORDINANCE`,
+SUM(i.`TOTALSURCHARGE`) `TOTALSURCHARGE`,
+SUM(i.`GRANDTOTAL`) `GRANDTOTAL`,
+`movie_date`,
+`ticket_datetime`,
+`userid`,
+`system_value`,
+`reportname`,
+#(COUNT(i.`QTY`) * `FOOD`) `FOOD`,
+SUM(i.`FOOD`) `FOOD`,
+`PatronId`,
+`CinemaOrdinal`
 
 FROM `tbl_rp01` i
 GROUP BY i.PatronId
