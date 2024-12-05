155d485a84f5e8cfb1f7fcef994fe57cf6edd286
Lambert Galit <lbg.globagility@gmail.com>
2024-12-05 13:03:18 +0800

    implement the DependencyInjection on Ticketing solution

---
+++ Paradiso/EF/DependencyInjectionHelper.cs
@@ -0,0 +1,31 @@
+﻿using Cinex.Core.Interfaces.DomainServices;
+using Cinex.WinForm;
+using CommonLibrary;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Paradiso.EF
+{
+    internal class DependencyInjectionHelper : DependencyInjection
+    {
+        public static string CONNECTION_STRING = CommonUtility.ConnectionString;
+
+        private readonly ServiceProvider _mainServiceProvider;
+
+        public DependencyInjectionHelper() : base(connectionString: CONNECTION_STRING)
+        {
+            _mainServiceProvider = MainServiceProvider;
+        }
+
+        public static ICinemaDataService GetCinemaDataService => NewInstance.MainServiceProvider.GetRequiredService<ICinemaDataService>();
+
+        public static ICinemaPatronDataService GetCinemaPatronDataService => NewInstance.MainServiceProvider.GetRequiredService<ICinemaPatronDataService>();
+
+        public static ICinemaPatronDefaultDataService GetCinemaPatronDefaultDataService => NewInstance.MainServiceProvider.GetRequiredService<ICinemaPatronDefaultDataService>();
+
+        public static IPatronDataService GetPatronDataService => NewInstance.MainServiceProvider.GetRequiredService<IPatronDataService>();
+
+        public static IEwalletDataService GetEwalletDataService => NewInstance.MainServiceProvider.GetRequiredService<IEwalletDataService>();
+
+        public static DependencyInjectionHelper NewInstance => new DependencyInjectionHelper();
+    }
+}
